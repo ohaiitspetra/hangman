e38777792b1563df31eb5f9f80c06c8a9a2f9374
petranassereddin <petralamees@gmail.com>
2019-02-22 21:06:48 -0500

    another oof

--- hangman.py
+++ hangman.py
@@ -8,25 +8,24 @@ def hang():
     for i, v in enumerate(sequence, start=1):
         for k in range(v):
             gallows[i] += next(parts)
-            yield '/n'.join(gallows)
-        raise StopIteration
+            yield '\n'.join(gallows)
+    raise StopIteration
 
 
 def play():
-
-        guess = input(f'Choose a letter!:')
-        print(guess)
-        with open('words.csv') as file:
+    guess = input(f'Choose a letter!:')
+    print(guess)
+    with open('words.csv') as file:
             data = list(file)
             word_list = [data[:-1] for word in data if len(word) > 4]
             secret_word = random.choice(word_list)
-            progress = ['_' for letter in secret_word]
-        if guess in secret_word:
+            progress = [print('_')for letter in secret_word]
+    if guess in secret_word:
             for idx, letter in enumerate(secret_word):
                 if letter == guess:
                     progress[idx] = guess
-        else:
-            return next(hang())
+    if guess not in secret_word:
+        print(next(hang()))
 
     # if all hangman parts are on the gallows:
     # print "Game over"
